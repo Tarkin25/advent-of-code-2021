242c206d27972452bdab837003922303afeccfe8
Tarkin25 <severin.weigold@gmx.ch>
2021-12-03 15:54:13 +0100

    day 3 - puzzle 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -3,36 +3,6 @@ use std::io::{BufRead, BufReader};
 
 type Number = u32;
 
-struct PowerConsumption {
-    gamma_rate: Number,
-    epsilon_rate: Number,
-}
-
-impl PowerConsumption {
-    fn new(numbers: Vec<Number>, significant_bits: usize) -> Self {
-        let mut gamma_rate: Number = 0;
-        let mut epsilon_rate: Number = 0;
-
-        fn apply(common_bit: CommonBit, number: &mut Number, position: usize) {
-            match common_bit {
-                CommonBit::Zero => *number |= 0 << position,
-                CommonBit::One => *number |= 1 << position,
-                CommonBit::Equal => {}
-            }
-        }
-
-        for position in 0..significant_bits {
-            apply(CommonBit::most_at(numbers.iter(), position), &mut gamma_rate, position);
-            apply(CommonBit::least_at(numbers.iter(), position), &mut epsilon_rate, position);
-        }
-
-        Self {
-            gamma_rate,
-            epsilon_rate,
-        }
-    }
-}
-
 #[derive(Copy, Clone)]
 enum CommonBit {
     Zero,
@@ -76,6 +46,48 @@ impl CommonBit {
     }
 }
 
+fn oxygen_generator_rating(numbers: Vec<Number>, significant_bits: usize) -> Number {
+    determine_rating(numbers, significant_bits, |numbers, position| {
+        if let CommonBit::Zero = CommonBit::most_at(numbers.iter(), position) {
+            0
+        } else {
+            1
+        }
+    })
+}
+
+fn co2_scrubber_rating(numbers: Vec<Number>, significant_bits: usize) -> Number {
+    determine_rating(numbers, significant_bits, |numbers, position| {
+        if let CommonBit::One = CommonBit::least_at(numbers.iter(), position) {
+            1
+        } else {
+            0
+        }
+    })
+}
+
+fn determine_rating<F>(mut numbers: Vec<Number>, significant_bits: usize, bit_criteria_provider: F) -> Number
+where
+    F: Fn(&[Number], usize) -> Number
+{
+    let mut position = significant_bits;
+
+    while numbers.len() > 1 {
+        {
+            let position = position -1;
+            let bit_criteria = bit_criteria_provider(&numbers, position);
+
+            numbers = numbers.into_iter()
+                .filter(|number| number >> position & 1 == bit_criteria)
+                .collect();
+        }
+
+        position -= 1;
+    }
+
+    numbers[0]
+}
+
 fn main() {
     let numbers: Vec<Number> = File::open("./input.txt")
         .map(BufReader::new)
@@ -85,9 +97,10 @@ fn main() {
         .map(|line| Number::from_str_radix(line.as_str(), 2).unwrap())
         .collect();
 
-    let PowerConsumption { gamma_rate, epsilon_rate } = PowerConsumption::new(numbers, 12);
+    let oxygen_generator_rating = oxygen_generator_rating(numbers.clone(), 12);
+    let co2_scrubber_rating = co2_scrubber_rating(numbers, 12);
 
-    println!("{}", gamma_rate * epsilon_rate);
+    println!("{}", oxygen_generator_rating * co2_scrubber_rating);
 }
 
 #[cfg(test)]
@@ -95,7 +108,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn oxygen_generator_rating_works() {
         let numbers: Vec<Number> = vec![
             0b_00100,
             0b_11110,
@@ -111,9 +124,26 @@ mod tests {
             0b_01010,
         ];
 
-        let PowerConsumption { gamma_rate, epsilon_rate } = PowerConsumption::new(numbers, 5);
+        assert_eq!(oxygen_generator_rating(numbers, 5), 23);
+    }
 
-        assert_eq!(gamma_rate, 0b_10110);
-        assert_eq!(epsilon_rate, 0b_01001);
+    #[test]
+    fn co2_scrubber_rating_works() {
+        let numbers: Vec<Number> = vec![
+            0b_00100,
+            0b_11110,
+            0b_10110,
+            0b_10111,
+            0b_10101,
+            0b_01111,
+            0b_00111,
+            0b_11100,
+            0b_10000,
+            0b_11001,
+            0b_00010,
+            0b_01010,
+        ];
+
+        assert_eq!(co2_scrubber_rating(numbers, 5), 10);
     }
 }
