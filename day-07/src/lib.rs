007e7f370311e3dd58b53753231fa43e1e404008
Tarkin25 <severin.weigold@gmx.ch>
2021-12-07 20:33:41 +0100

    day 07 - performance improvements + variability

--- day-07/src/lib.rs
+++ day-07/src/lib.rs
@@ -3,25 +3,45 @@ use crate::minmax::MinMax;
 
 mod minmax;
 
-pub fn optimal_position(numbers: Vec<usize>) -> Option<(usize, usize)> {
+pub fn optimal_position<F: FuelCalculator>(numbers: Vec<usize>) -> Option<(usize, usize)> {
     let (min, max) = numbers.iter().min_max()?;
 
     (*min..=*max)
-        .map(|position| (position, fuel_consumption(&numbers, position)))
+        .map(|position| (position, fuel_consumption::<F, _>(&numbers, position)))
         .min_by_key(|(_, fuel)| *fuel)
 }
 
-fn fuel_consumption<'a>(numbers: impl IntoIterator<Item = &'a usize>, x: usize) -> usize {
-    numbers.into_iter().map(|n| n.fuel_to(x)).sum()
+fn fuel_consumption<'a, F: FuelCalculator, I: IntoIterator<Item = &'a usize>>(numbers: I, x: usize) -> usize {
+    numbers.into_iter().map(|n| F::fuel(*n, x)).sum()
 }
 
-trait FuelTo {
-    fn fuel_to(self, target: Self) -> Self;
+pub trait FuelCalculator {
+    fn fuel(position: usize, target: usize) -> usize;
 }
 
-impl FuelTo for usize {
-    fn fuel_to(self, target: Self) -> Self {
-        (1..=self.diff(target)).sum()
+pub struct Part1;
+
+impl FuelCalculator for Part1 {
+    fn fuel(position: usize, target: usize) -> usize {
+        position.diff(target)
+    }
+}
+
+pub struct Part2ManualSum;
+
+impl FuelCalculator for Part2ManualSum {
+    fn fuel(position: usize, target: usize) -> usize {
+        (1..=position.diff(target)).sum()
+    }
+}
+
+pub struct Part2Triangular;
+
+impl FuelCalculator for Part2Triangular {
+    fn fuel(position: usize, target: usize) -> usize {
+        let diff = position.diff(target) as f64;
+        let fuel = (diff.powi(2) + diff) / 2.0;
+        fuel as usize
     }
 }
 
@@ -53,15 +73,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn optimal_position_works() {
+    fn optimal_position_part_1() {
         let positions = vec![16,1,2,0,4,2,7,1,2,14];
-        assert_eq!(Some((5, 168)), optimal_position(positions));
+        assert_eq!(Some((2, 37)), optimal_position::<Part1>(positions));
     }
 
     #[test]
-    fn fuel_consumption_works() {
+    fn optimal_position_part_2() {
+        let positions = vec![16,1,2,0,4,2,7,1,2,14];
+        assert_eq!(Some((5, 168)), optimal_position::<Part2Triangular>(positions));
+    }
+
+    #[test]
+    fn fuel_consumption_part_1() {
         let positions = vec![1,2,4,5];
-        assert_eq!(fuel_consumption(&positions, 3), 8);
-        assert_eq!(fuel_consumption(&positions, 1), 17);
+        assert_eq!(fuel_consumption::<Part1, _>(&positions, 3), 6);
+        assert_eq!(fuel_consumption::<Part1, _>(&positions, 1), 8);
+    }
+
+    #[test]
+    fn fuel_consumption_part_2_manual_sum() {
+        let positions = vec![1,2,4,5];
+        assert_eq!(fuel_consumption::<Part2ManualSum, _>(&positions, 3), 8);
+        assert_eq!(fuel_consumption::<Part2ManualSum, _>(&positions, 1), 17);
+    }
+
+    #[test]
+    fn fuel_consumption_part_2_triangular() {
+        let positions = vec![1,2,4,5];
+        assert_eq!(fuel_consumption::<Part2Triangular, _>(&positions, 3), 8);
+        assert_eq!(fuel_consumption::<Part2Triangular, _>(&positions, 1), 17);
     }
 }
--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use day_07::optimal_position;
+use day_07::{optimal_position, Part1, Part2Triangular};
 
 fn main() {
     let numbers: Vec<usize> = fs::read_to_string("./input.txt").unwrap()
@@ -7,8 +7,9 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let (position, fuel) = optimal_position(numbers).unwrap();
+    let (position, fuel) = optimal_position::<Part1>(numbers.clone()).unwrap();
+    println!("[Part 1] position: {}, fuel: {}", position, fuel);
 
-
-    println!("position: {}, fuel: {}", position, fuel);
+    let (position, fuel) = optimal_position::<Part2Triangular>(numbers).unwrap();
+    println!("[Part 2] position: {}, fuel: {}", position, fuel);
 }
